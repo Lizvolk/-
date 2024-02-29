0adfda6630e1e2e14a131959f715c45a9c50aa3c
Lizvolk <114811597+Lizvolk@users.noreply.github.com>
2024-02-29 17:02:48 +0300

    1 практика

---
+++ Department.cs
@@ -0,0 +1,12 @@
+﻿namespace VP_Pract1
+{
+    public class Department
+    {
+       public int Id;
+       public int Name;
+       public void Insert() { }
+       public void Update() { }
+       public void Delete() { }
+
+    }
+}
---
+++ Report.cs
@@ -0,0 +1,11 @@
+﻿namespace VP_Pract1
+{
+    public class Report
+    {
+        public int Code;
+        public int Date;
+
+        public void View() { }
+        public void Create() { }
+    }
+}
