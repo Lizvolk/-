fc8e4319012397555738b871be761e77960023ca
Lizvolk <114811597+Lizvolk@users.noreply.github.com>
2024-03-15 18:07:21 +0300

    ..

---
+++ 3 практ/VP_Pract3/StringExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+
+namespace VP_Pract3
+{
+    public struct StringInfo
+    {
+
+        private int lenght;
+        private int digitCount;
+        private int letterCount;
+        public int Length
+        {
+            get
+            {
+                return lenght;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    lenght = value;
+                }
+                else
+                {
+                    throw new Exception("Length must be greater than 0");
+                }
+            } 
+        }
+        public int DigitCount 
+        {
+            get
+            {
+                return digitCount;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    digitCount = value;
+                }
+                else
+                {
+                    throw new Exception("DigitCount must be greater than 0");
+                }
+            }
+        }
+        public int LetterCount 
+        {
+            get
+            {
+                return letterCount;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    letterCount = value;
+                }
+                else
+                {
+                    throw new Exception("LetterCount must be greater than 0");
+                }
+            }
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static StringInfo GetStringInfo(this string input)
+        {
+            StringInfo info = new StringInfo
+            {
+                Length = input.Length,
+                DigitCount = input.Count(char.IsDigit),
+                LetterCount = input.Count(char.IsLetter)
+            };
+
+            return info;
+        }
+    }
+}
---
+++ 3 практ/VP_Pract3UI/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Globalization;
+
+namespace VP_Pract3
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string text = "Практическая работа 3 по вп, Егорова,Волкова 12345";
+            Console.WriteLine(text.ToString());
+            StringInfo info = text.GetStringInfo();
+            
+            Console.WriteLine($"Длина: {info.Length}");
+            Console.WriteLine($"Число цифр: {info.DigitCount}");
+            Console.WriteLine($"Число букв: {info.LetterCount}");
+        }
+    }
+}
